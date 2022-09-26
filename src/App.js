0e089a8a24a2dce70d2cc70e266e30b2f78cd53e
Anil Gautam <agmca55@gmail.com>
2022-09-26 09:43:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React test
+          This website is developed by Anil
         </a>
       </header>
     </div>
